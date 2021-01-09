c93e34b8b8031a3fa22b5d8e26ecd581ec5f437f
Eylül <eyluldicleyurdakul@gmail.com>
2021-01-09 18:17:15 +0100

    Make the project ready for production deployment

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adb2989a02bff41c4b0ce06eb96d0ee9-2014280564.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
